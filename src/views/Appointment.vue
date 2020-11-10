e953e9b85713bdee81a8aa02f0136c8ebc936ca6
nikctasma <nikctasma@gmail.com>
2020-11-11 00:14:44 +0800

    Initial commit

---
+++ src/views/Appointment.vue
@@ -0,0 +1,122 @@
+<template>
+    <div>
+        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+        <div class="container">
+            <h2>Barber Schedule</h2>
+            <div id="app">
+                <date-picker v-model="date" lang="en" type="date" format="DD-MM-YYYY-dd"></date-picker>
+            </div>
+
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>GMT</th>
+                            <th>Barber Akmal</th>
+                            <th>Barber Arif</th>
+                            <th>Barber Ahmad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>10.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>11.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>12.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>13.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>14.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>15.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>16.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>17.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>18.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>19.00</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        </base-header>
+    </div>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
+    export default{
+        name: 'App',
+        components:{
+            DatePicker
+        },
+        data(){
+            return {
+                date: ''
+            }
+        }
+    }
+
+</script>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
